f2c591f3a567fbb040ab462ca4334992a1fb6be1
Kyle Valdillez <13121777+kvaldillez@users.noreply.github.com>
2018-04-19 23:15:54 -0700

    Added MongoDB and mongoose for user storage

---
+++ services/passport.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // User with this google ID exists
+          done(null, existingUser);
+        } else {
+          // User with this google ID does not exist, create a new one
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
